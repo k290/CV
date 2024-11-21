f65404bee693f4f57407ef344aa2ab474d943d74
Matthew <holmesmatt907@gmail.com>
2024-11-21 17:53:41 +0200

    valid typescript

--- components/main-header.ts
+++ components/main-header.ts
@@ -1,6 +1,27 @@
-﻿import { FASTElement } from "@microsoft/fast-element";
+﻿import { FASTElement, attr, css, html } from "@microsoft/fast-element";
 
+const template = html`<div>${x => x.text}!</div>`
 
-class HelloWorld extends FASTElement {
+/**
+ * Create CSS styles using the css tag template literal
+ */
+const styles = css`
+    :host {
+      border: 1px solid blue;
+    }
 
+    span {
+      color: red;
+    }
+`;
+
+class MainHeader extends FASTElement {
+    @attr
+    text!: string;
 }
+
+MainHeader.define({
+    name: "main-jeader",
+    template,
+    styles,
+});
